f27f22d52b9d600a1ba2d0f4f7c5a6f816bde566
Martin Briceno <martin.briceno@live.com>
2020-07-15 17:22:53 -0400

    [while loops]

--- app.js
+++ app.js
@@ -9,3 +9,20 @@ console.log(favNum);
 
 var teams = ['Rockets', 'Lakers', 'Warriors', 'Clippers', 'Blazers', 'Celtics', 'Nets']
 console.log(teams);
+
+// while loops
+var num = 10;
+var otherNum = 1
+while (otherNum < num) {
+    console.log(otherNum);
+    otherNum +=1;
+
+}
+
+var numTwo = 20;
+var myOtherNum = 10;
+
+while (myOtherNum < numTwo) {
+    console.log(myOtherNum);
+    myOtherNum += 1;
+}
