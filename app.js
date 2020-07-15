10ea5f2fbe2ceffa9c1ed4ac6a50a6bc57b0c1dd
Martin Briceno <martin.briceno@live.com>
2020-07-15 17:03:51 -0400

    [arrays] add fav teams

---
+++ app.js
@@ -0,0 +1,11 @@
+var name = "Martin";
+console.log(name);
+
+var myLoc = "Boston";
+console.log(myLoc);
+
+var favNum = 7;
+console.log(favNum);
+
+var teams = ['Rockets', 'Lakers', 'Warriors', 'Clippers', 'Blazers', 'Celtics', 'Nets']
+console.log(teams);
